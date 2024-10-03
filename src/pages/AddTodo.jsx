eb5860069f658a8641587eb822167c1ee5efee6d
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-03 15:48:48 +0000

    thursday - after class

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,14 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddTodo = () => {
     const navigate = useNavigate();
 
-    const saveTodo = (event) => {
+    const saveTodo = async (event) => {
         event.preventDefault();
-
+        // Collecting form input...
+        const formData = new FormData(event.target)
+        
         // Post data to todo api...
-        // Goto the homepage...
+        await axios.post('https://todo-api-i7c7.onrender.com/todos',{
+            title: formData.get('todo')
+        })
 
+        // Goto the homepage...
         navigate('/');
 
     }
@@ -16,7 +22,7 @@ const AddTodo = () => {
         <>
             <h1>New todo's here...</h1>
             <form onSubmit={saveTodo}>
-                <input type="text" name="task" id="task" placeholder="Enter any activity..." />
+                <input type="text" name="todo" id="todo" placeholder="Enter any activity..." />
                 <input type="submit" value="Add" />
             </form>
         </>
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,18 +1,42 @@
 // NB: pages will have large components that will most likely cover the entire page...
 
 // 
+import { useEffect, useState } from "react";
 import TodoTile from "../components/TodoTile";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Todos = () => {
+    // 1.  Declaring a state to store todos...
+    const [todos, setTodos] = useState([]);
+    // where todos = the variable...
+    // setTodos = what we will use to change the variables...
+
+    // 2. Defining a function to get todos...
+    const getTodos = async () => {
+        // use axioms to get todos
+        const response = await axios.get('https://todo-api-i7c7.onrender.com/todos');
+        console.log(response.data)
+        // update todo state
+        setTodos(response.data);
+    }
+
+    // 3. Call function with useEffect...
+    useEffect(() => {
+        getTodos();
+    }, []);
+
     return (
         <div>
             <h1>All todo's go here...</h1>
             <div>
-                <TodoTile title={'Try 2'} />
+                {todos.map((todo) => {
+                    return <TodoTile title={todo.title} key={todo._id}/>
+                })}
+                {/* <TodoTile title={'Try 2'} />
                 <TodoTile title={'Try 3'} />
                 <TodoTile title={'Try 4'} />
-                <TodoTile title={'Try 5'} />
+                <TodoTile title={'Try 5'} /> */}
             </div>
             <Link to={'/add'}>Add Todo</Link>
         </div>
